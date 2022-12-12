99fdc301889011e7b73bdb0692828355d1d0ace3
Ivan Ho <ivandaho@gmail.com>
2022-12-12 09:53:28 +0800

    feat: update_capabilities -> default_capabilities

--- lua/config.lua
+++ lua/config.lua
@@ -40,7 +40,7 @@ end
 local nvim_lsp = require'lspconfig'
 
 local servers = { 'tsserver', 'eslint', 'gopls' }
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 for _, lsp in pairs(servers) do
 	require('lspconfig')[lsp].setup{
 		on_attach = on_attach,
