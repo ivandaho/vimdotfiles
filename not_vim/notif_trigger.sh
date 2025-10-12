2ae1568f6165ef99afcf2c9ba3dda831a6b82f1e
Ivan Ho <ivandaho@gmail.com>
2025-10-12 18:21:26 +0800

    feat: support custom message for NotifOverlay

--- not_vim/notif_trigger.sh
+++ not_vim/notif_trigger.sh
@@ -2,7 +2,9 @@
 
 CURRENT_WINDOW="$(yabai -m query --windows --window | jq -r '.app')"
 YABAI_RESULT="$(yabai -m query --windows)"
-# RESULT=$(echo "$YABAI_RESULT" | jq -r "map(select(.app == \"$CURRENT_WINDOW\")) | map({id, title, \"has-focus\"}) | map(if .\"has-focus\" == true then \"> \" + .title else \"  \" + .title end) | .[]")
 RESULT=$(echo "$YABAI_RESULT" | jq -r "map(select(.app == \"$CURRENT_WINDOW\")) | map({id, app, title, hasFocus: .\"has-focus\", isMinimized: .\"is-minimized\"}) | tojson")
 
-echo -n "$RESULT" | nc -U /tmp/notif-overlay.sock
+
+RESULT2=$(echo "$RESULT" | jq -r "{yabaiWindows: ., type: \"message\", message: \"$RESULT2\"}")
+
+echo -n "$RESULT2" | nc -U /tmp/notif-overlay.sock
