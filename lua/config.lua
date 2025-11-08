45587ea750e3ee942f0ba8dc869e75d57e3cefa5
Ivan Ho <ivandaho@gmail.com>
2025-11-08 17:12:11 +0800

    chore: lint lua file

--- lua/config.lua
+++ lua/config.lua
@@ -1,22 +1,20 @@
-
-local opts = { noremap=true, silent=true }
+local opts = { noremap = true, silent = true }
 local on_attach = function(client, bufnr)
-
-	vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gh', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gw', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<c-j>', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<c-k>', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gj',    '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gm',    '<cmd>call Black()<CR>', opts)
-	-- if client == 'gopls' then
-	-- 	vim.api.nvim_buf_set_keymap(bufnr, 'n', '\\q',    ':GoFmt<CR>', {})
-	-- end
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gj', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gm', '<cmd>call Black()<CR>', opts)
+  -- if client == 'gopls' then
+  -- 	vim.api.nvim_buf_set_keymap(bufnr, 'n', '\\q',    ':GoFmt<CR>', {})
+  -- end
 
-	vim.api.nvim_exec(
-	[[
+  vim.api.nvim_exec(
+    [[
 		function! Highlight()
 			call v:lua.vim.lsp.buf.document_highlight()
 			call v:lua.vim.lsp.util.buf_clear_references()
@@ -25,21 +23,20 @@ local on_attach = function(client, bufnr)
 
 		autocmd CursorHold * call Highlight()
 	]], false)
-	-- vim.api.nvim_set_hl(0, 
-
+  -- vim.api.nvim_set_hl(0,
 end
 
 local servers = { 'ts_ls', 'eslint', 'gopls', 'pyright', 'sourcekit' }
 local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 for _, lsp in pairs(servers) do
-	vim.lsp.config(lsp, {
-		on_attach = on_attach,
-		flags = {
-			-- This will be the default in neovim 0.7+
-			debounce_text_changes = 150,
-		},
+  vim.lsp.config(lsp, {
+    on_attach = on_attach,
+    flags = {
+      -- This will be the default in neovim 0.7+
+      debounce_text_changes = 150,
+    },
     capabilities = capabilities
-	})
+  })
   vim.lsp.enable(lsp)
 end
 
@@ -94,40 +91,40 @@ vim.api.nvim_set_keymap('i', 'cncn', 'className=""i', opts)
 vim.api.nvim_set_keymap('i', 'fnfl', '() => {}i<enter>Oja', opts)
 vim.api.nvim_set_keymap('i', 'fncl', '() => ({})hi<enter>Oja', opts)
 
-require'nvim-treesitter.configs'.setup {
--- One of "all", "maintained" (parsers with maintainers), or a list of languages
+require 'nvim-treesitter.configs'.setup {
+  -- One of "all", "maintained" (parsers with maintainers), or a list of languages
   ensure_installed = {
-		"bash",
-		"css",
-		"dockerfile",
-		"fish",
-		"go",
-		"gomod",
-		"gowork",
-		"graphql",
-		"html",
-		"http",
-		"java",
-		"javascript",
-		"jsdoc",
-		"json",
-		"json5",
-		"jsonc",
-		"lua",
-		"make",
-		"markdown",
-		"pug",
-		"python",
-		"ql",
-		"regex",
-		"scss",
-		"tsx",
-		"typescript",
-		"vim",
-		"vue",
-		"yaml",
+    "bash",
+    "css",
+    "dockerfile",
+    "fish",
+    "go",
+    "gomod",
+    "gowork",
+    "graphql",
+    "html",
+    "http",
+    "java",
+    "javascript",
+    "jsdoc",
+    "json",
+    "json5",
+    "jsonc",
+    "lua",
+    "make",
+    "markdown",
+    "pug",
+    "python",
+    "ql",
+    "regex",
+    "scss",
+    "tsx",
+    "typescript",
+    "vim",
+    "vue",
+    "yaml",
     "terraform",
-	},
+  },
   -- Install languages synchronously (only applied to `ensure_installed`)
   sync_install = false,
 
@@ -151,79 +148,80 @@ require'nvim-treesitter.configs'.setup {
 
 
 -- Setup nvim-cmp.
-local cmp = require'cmp'
+local cmp = require 'cmp'
 
 cmp.setup({
-	snippet = {
-		-- REQUIRED - you must specify a snippet engine
-		expand = function(args)
-			-- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-			-- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-			-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-			-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-		end,
-	},
-	window = {
-		completion = cmp.config.window.bordered(),
-		documentation = cmp.config.window.bordered(),
-	},
-	mapping = cmp.mapping.preset.insert({
-		['<Tab>'] = function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			else
-				fallback()
-			end
-		end,
-		['<S-Tab>'] = function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			else
-				fallback()
-			end
-		end,
-		['<C-f>'] = cmp.mapping.scroll_docs(4),
-		['<C-e>'] = cmp.mapping.abort(),
+  snippet = {
+    -- REQUIRED - you must specify a snippet engine
+    expand = function(args)
+      -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+      -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+    end,
+  },
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      else
+        fallback()
+      end
+    end,
+    ['<S-Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      else
+        fallback()
+      end
+    end,
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-e>'] = cmp.mapping.abort(),
 
-		['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-	}),
-	sources = cmp.config.sources({
-		{ name = 'nvim_lsp' },
-		-- { name = 'vsnip' }, -- For vsnip users.
-		-- { name = 'luasnip' }, -- For luasnip users.
-		-- { name = 'ultisnips' }, -- For ultisnips users.
-		-- { name = 'snippy' }, -- For snippy users.
-	}, {
-		{ name = 'buffer' ,
-			option = {
-				get_bufnrs = function()
-					return vim.api.nvim_list_bufs()
-				end
-			}
-	},
-	}),
-	confirmation = {
-		get_commit_characters = function(commit_characters)
-			return {}
-		end
-	}
+    ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    -- { name = 'vsnip' }, -- For vsnip users.
+    -- { name = 'luasnip' }, -- For luasnip users.
+    -- { name = 'ultisnips' }, -- For ultisnips users.
+    -- { name = 'snippy' }, -- For snippy users.
+  }, {
+    {
+      name = 'buffer',
+      option = {
+        get_bufnrs = function()
+          return vim.api.nvim_list_bufs()
+        end
+      }
+    },
+  }),
+  confirmation = {
+    get_commit_characters = function(commit_characters)
+      return {}
+    end
+  }
 })
 
 -- Set configuration for specific filetype.
 cmp.setup.filetype('gitcommit', {
-	sources = cmp.config.sources({
-		{ name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-	}, {
-		{ name = 'buffer' },
-	})
+  sources = cmp.config.sources({
+    { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+  }, {
+    { name = 'buffer' },
+  })
 })
 
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline('/', {
-	mapping = cmp.mapping.preset.cmdline(),
-	sources = {
-		{ name = 'buffer' }
-	}
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
 })
 
 -- Default options:
@@ -245,13 +243,13 @@ require("gruvbox").setup({
   invert_tabline = false,
   invert_intend_guides = false,
   inverse = true, -- invert background for search, diffs, statuslines and errors
-  contrast = "", -- can be "hard", "soft" or empty string
+  contrast = "",  -- can be "hard", "soft" or empty string
   palette_overrides = {},
   -- overrides = {
-	-- 	["LspReferenceRead"] = { bg = "#458599" },
-	-- 	["LspReferenceText"] = { bg = "#ebdbb2" },
-	-- 	["LspReferenceWrite"] = { bg = "#ff0000" },
-	-- },
+  -- 	["LspReferenceRead"] = { bg = "#458599" },
+  -- 	["LspReferenceText"] = { bg = "#ebdbb2" },
+  -- 	["LspReferenceWrite"] = { bg = "#ff0000" },
+  -- },
   dim_inactive = false,
   transparent_mode = false,
 })
